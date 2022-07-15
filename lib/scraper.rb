9f2d4b738d2bd3fcbfc70c01f0a418e2f8fa8719
Janice Chan <janice.chan1288@gmail.com>
2022-07-15 13:35:03 -0400

    commit tree b04792621574a6b9cf9f5275461d85f3713fb7c6

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,13 @@
-require 'nokogiri'
+# gem to parse HTML and collect data. Allows us to treat a huge string of HTML into a series of nested objects
+require 'nokogiri' 
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
+# stores the html of the flatiron website in variable html
+# open method returns string of HTML
+
+# Nokogiri::HTML(html)
+# this method takes html (string of HTML) and converts it to NodeSet (nested "nodes")
+
+doc = Nokogiri::HTML(html)
+doc.css(".site-logo logo-light")
